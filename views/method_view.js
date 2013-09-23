878107ea6b9d4fac8656c19ce6570a8337b9137a
Olav Stetter <olav.stetter@googlemail.com>
2013-09-23 11:27:30 +0200

    Hidden methods are now also greyed out.

--- views/method_view.js
+++ views/method_view.js
@@ -8,26 +8,6 @@ app.ReconstructionMethodView = Backbone.View.extend({
     _.bindAll(this, 'filter_matching_state_has_changed', 'hide', 'show');
   },
   
-  // render: function() {
-  //   // alert("render called for method view of "+this.model.get('name'));
-  //   // assemble properties list string
-  //   var prop = _.reduce(this.model.get("properties"), function(memo, value, key) {
-  //     return memo+"<li>"+key+": "+(value ? "Yes" : "No")+"</li>";
-  //   }, "");
-  //   
-  //   this.$el.html( this.template({
-  //     "name" : this.model.get("name"),
-  //     "properties_list" : prop,
-  //     "reference_url" : this.model.get("reference_url")
-  //   }));
-  //   
-  //   // switch on 'verified' flag
-  //   if(this.model.get("verified_by_author")) {
-  //     // alert("DEBUG: method "+this.model.get("name")+" was verified by the author.");
-  //     this.$el.find('.verified_symbol').show();
-  //   }
-  //   return this;
-  // },
   render: function() {
     // alert("DEBUG: rendering category "+this.model.get("name"));
     this.$el.html( this.template(this.model.toJSON()) );
@@ -45,12 +25,17 @@ app.ReconstructionMethodView = Backbone.View.extend({
   
   hide: function() {
     // alert("hide");
+    this.$el.addClass('hidden-method');
     this.$el.fadeTo(500, 0.2);
+    // this.$el.fadeTo(500, 0.2, function() {
+    //   $(this).addClass('hidden-method');
+    // });
   },
   
   show: function() {
     // alert("show");
     this.$el.fadeTo(500, 1);
+    this.$el.removeClass('hidden-method');
   }
   
 });
